421da78a1252a3d19b6eabe09788fff4e054f1cf
matthewg372 <59273107+matthewg372@users.noreply.github.com>
2020-04-30 11:11:12 -0500

    login is working successfully

--- app.py
+++ app.py
@@ -4,10 +4,27 @@ from resources.products import products
 import models
 from resources.users import users
 
+from flask_login import LoginManager
+
 DEBUG=True
 PORT=8000
 
 app = Flask(__name__)
+app.secret_key = "ajhskdjasdhkasdjhkaiudhajugjhgdjhsgfhgjskdhjkfhjashd"
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+	try:
+		return models.User.get_by_id(user_id)
+	except models.DoesNotExist:
+		return None 
+
+
+
+
+
 
 app.register_blueprint(products, url_prefix='/api/v1/products')
 app.register_blueprint(users, url_prefix='/api/v1/users')
