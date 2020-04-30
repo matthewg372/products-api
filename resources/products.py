3199f4c348617102d46942e483eb6b081726b730
matthewg372 <59273107+matthewg372@users.noreply.github.com>
2020-04-30 11:17:03 -0500

    products create route made

--- resources/products.py
+++ resources/products.py
@@ -1,7 +1,7 @@
 import models
 from flask import Blueprint, request, jsonify
 from playhouse.shortcuts import model_to_dict
-# from flask_login import current_user
+from flask_login import current_user
 
 products = Blueprint('products', 'products')
 
@@ -10,20 +10,21 @@ def connected():
 	return 'connected to Blueprint'
 
 
-# @products.route('/', methods=['POST'])
-# def create_products():
-# 	payload = request.get_json()
-# 	new_products = models.Product.create(
-# 		name=payload['name'],
-# 		user=current_user.id,
-# 		flavors=payload['flavors'],
-# 		quantity=payload['quantity'],
-# 		price=payload['price']
+@products.route('/', methods=['POST'])
+def create_products():
+	payload = request.get_json()
+	new_products = models.Product.create(
+		name=payload['name'],
+		user=current_user.id,
+		flavors=payload['flavors'],
+		quantity=payload['quantity'],
+		price=payload['price']
 
-# 	)
-# 	product_dict = model_to_dict(new_products)
-# 	return jsonify(
-# 		data=product_dict,
-# 		message="succesfully created product",
-# 		status=200
-# 	), 200
+	)
+	product_dict = model_to_dict(new_products)
+	product_dict['user'].pop('password')
+	return jsonify(
+		data=product_dict,
+		message="succesfully created product",
+		status=200
+	), 200
